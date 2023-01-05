81932353bf25887de7b5ba745839baab323c1f65
TranQuyen <118921032+quyendepzai123@users.noreply.github.com>
2023-01-05 21:05:25 +0700

    'home-screen'

---
+++ src/components/ButtonCategory/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+import styles from '../../screens/HomeScreen/style';
+
+type Props = {
+  onPress: Function;
+  title: string;
+  isSelected: boolean;
+};
+
+const Button = (props: Props) => {
+  const { onPress, title, isSelected } = props;
+  return (
+    <View>
+      <Pressable
+        onPress={() => onPress()}
+        style={[styles.ButtonTabOpacity, isSelected === true ? styles.backGroundButton : null]}
+      >
+        <Text style={isSelected === true ? styles.textColorSelected : null}>{title}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Button;
