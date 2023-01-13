ca4f6fe632672a8632359163506d64d71161f67e
ryanvo162 <ryanvo.0162@gmail.com>
2023-01-14 02:10:00 +0700

    fix: eslint

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '*': './*',
+          },
+        },
+      ],
+    ],
   };
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { View, Text } from 'react-native';
+import Home from 'screens/Home';
+import styles from 'styles/global';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test git</Text>
+      <Home />
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
