d9bf1f1ca1b3429ecca1125374e368087a4ee3a4
ryanvo162 <ryanvo.0162@gmail.com>
2023-01-14 03:28:54 +0700

    feat: splash screen and icon app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,49 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import * as SystemUI from 'expo-system-ui';
 import { View, Text } from 'react-native';
 import Home from 'screens/Home';
 import styles from 'styles/global';
 
+SplashScreen.preventAutoHideAsync();
+SystemUI.setBackgroundColorAsync('#eafafc');
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(Entypo.font);
+        await SystemUI.getBackgroundColorAsync();
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Home />
       <Text>Hello World</Text>
+      <Entypo name="rocket" size={30} />
       <StatusBar style="auto" />
     </View>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
