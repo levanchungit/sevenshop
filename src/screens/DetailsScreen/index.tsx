fb324cef331cd74292568640cc6715321c345beb
Khoa <101702506+Khoattdps19321@users.noreply.github.com>
2023-02-13 22:31:55 +0700

    detail screen design tabview add libary pager-view & tab-view

--- src/screens/DetailsScreen/index.tsx
+++ src/screens/DetailsScreen/index.tsx
@@ -1,19 +1,66 @@
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { Button, Text, View } from 'native-base';
-import { DetailRouteProp, MainScreenNavigationProp } from 'providers/navigation/types';
+import { useState } from 'react';
+import { Text, View, Flex, Pressable, Box } from 'native-base';
+import { useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => <View style={{ flex: 1, backgroundColor: '#ff4081' }} />;
+
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: '#673ab7' }} />;
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
 
 const DetailsScreen = () => {
-  const navigation = useNavigation<MainScreenNavigationProp>();
-  const route = useRoute<DetailRouteProp>();
-  const { name, birthYear } = route.params;
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: 'first',
+      title: 'Description',
+    },
+    {
+      key: 'second',
+      title: 'Review',
+    },
+  ]);
 
   return (
-    <View style={{ flex: 1, paddingTop: 12, paddingHorizontal: 10 }}>
-      <Text style={{ fontSize: 18, paddingBottom: 12 }}>Name: {name}</Text>
-      <Text style={{ fontSize: 18 }}>Birth Year: {birthYear}</Text>
-      <Button onPress={() => navigation.navigate('Cart')} style={{ marginTop: 12 }}>
-        Go to Profile
-      </Button>
+    <View style={{ flex: 1, paddingTop: 12, paddingHorizontal: 10, flexDirection: 'column' }}>
+      <Text fontSize="2xl" fontWeight="semibold">
+        Product Name
+      </Text>
+      <Flex direction="row" justifyContent="flex-start">
+        <Text fontSize="xl" fontWeight="semibold" color="red.600">
+          100.000đ
+        </Text>
+        <Text fontSize="md" strikeThrough color="gray.500">
+          400.000đ
+        </Text>
+      </Flex>
+      <TabView
+        navigationState={{
+          index,
+          routes,
+        }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
+      <Flex direction="row" justifyContent="space-between">
+        <Text fontSize="md" fontWeight="bold">
+          Recommend for you
+        </Text>
+        <Text fontSize="md" fontWeight="bold">
+          See all
+        </Text>
+      </Flex>
+      <Box shadow="9" borderLeftRadius="5" borderRightRadius="5">
+        <Pressable>
+          <Text>Hello</Text>
+        </Pressable>
+      </Box>
     </View>
   );
 };
