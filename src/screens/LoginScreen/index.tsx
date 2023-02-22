ebe5e6f42c55d4995c1b27fae05f0708e855dcf8
LeVanChung19319 <86992800+LeVanChung19319@users.noreply.github.com>
2023-02-22 11:29:14 +0700

    add component text, fix fonts

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -62,7 +62,18 @@ const LoginScreen = () => {
 
   return (
     <Center flex="1">
-      <Text variant="h1">Login Screen</Text>
+      <Text variant="h1">Heading 1</Text>
+      <Text variant="h2">Heading 2</Text>
+      <Text variant="h3">Heading 3</Text>
+      <Text variant="h4">Heading 4</Text>
+      <Text variant="title">Title</Text>
+      <Text variant="subtitle1">Subtitle 1</Text>
+      <Text variant="subtitle2">Subtitle 2</Text>
+      <Text variant="body1">Body 1</Text>
+      <Text variant="body2">Body 2</Text>
+      <Text variant="button">BUTTON</Text>
+      <Text variant="caption">Caption</Text>
+      <Text variant="overline">OVERLINE</Text>
       <Button variant="primary" endIcon={<></>}>
         Button
       </Button>
