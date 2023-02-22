d5cfa38fa9d6da57336c875d4f58df3401efe408
LeVanChung19319 <86992800+LeVanChung19319@users.noreply.github.com>
2023-02-22 11:59:33 +0700

    edit login screen call api

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Text, View, Image, Toast, Center, Button, VStack, Input, FormControl } from 'native-base';
-import { TextInput, TouchableOpacity } from 'react-native';
+import { Text, Toast, Center, Button, VStack, Input, FormControl } from 'native-base';
 import { clearAuthTokens, getRefreshToken } from 'react-native-axios-jwt';
-import * as Icon from 'react-native-feather';
 import { RefreshTokenPayload, SignInPayload } from 'interfaces/Auth';
 import { authAPI } from 'modules/api';
 import { AppNavigationProp } from 'providers/navigation/types';
@@ -71,21 +69,7 @@ const LoginScreen = (props: Props) => {
 
   return (
     <Center flex="1">
-      <Text variant="h1">Heading 1</Text>
-      <Text variant="h2">Heading 2</Text>
-      <Text variant="h3">Heading 3</Text>
-      <Text variant="h4">Heading 4</Text>
-      <Text variant="title">Title</Text>
-      <Text variant="subtitle1">Subtitle 1</Text>
-      <Text variant="subtitle2">Subtitle 2</Text>
-      <Text variant="body1">Body 1</Text>
-      <Text variant="body2">Body 2</Text>
-      <Text variant="button">BUTTON</Text>
-      <Text variant="caption">Caption</Text>
-      <Text variant="overline">OVERLINE</Text>
-      <Button variant="primary" endIcon={<></>}>
-        Button
-      </Button>
+      <Text variant={'h1'}>Login</Text>
       <VStack width="90%" mx="3" maxW="300px">
         <FormControl isRequired>
           <FormControl.Label
@@ -102,57 +86,37 @@ const LoginScreen = (props: Props) => {
             value={formData.email}
             onChangeText={(value) => setFormData({ ...formData, email: value })}
             style={styles.inputuser}
-            // placeholder="Enter your email/phone number"
           />
         </FormControl>
 
-        <View style={styles.viewPass}>
-          <Icon.Lock stroke="black" width={24} height={24} style={{ marginRight: 5 }} />
-          <TextInput
+        <FormControl isRequired>
+          <FormControl.Label
+            _text={{
+              bold: true,
+            }}
+          >
+            Password
+          </FormControl.Label>
+          <Input
+            placeholder="Enter your password"
+            autoComplete="email"
+            autoCapitalize="none"
             value={formData.password}
             onChangeText={(value) => setFormData({ ...formData, password: value })}
             style={styles.inputuser}
-            placeholder="Enter your password"
             secureTextEntry={true}
           />
-          <Icon.EyeOff stroke="black" width={24} height={24} />
-        </View>
-        <View style={{ width: '80%' }}>
-          <Text style={styles.txtFogot}>Forgot password?</Text>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={onSubmit}>
-          <Text style={{ fontSize: 14 }}>Login</Text>
-        </TouchableOpacity>
+        </FormControl>
 
-        <TouchableOpacity style={styles.button} onPress={logout}>
-          <Text style={{ fontSize: 14 }}>Logout</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button} onPress={fetchData}>
-          <Text style={{ fontSize: 14 }}>Me</Text>
-        </TouchableOpacity>
-
-        <Text style={{ fontSize: 14, marginTop: 30 }}>Or login with</Text>
-        <View style={styles.viewimage}>
-          <Image
-            alt="1"
-            style={{ width: 40, height: 40 }}
-            source={{
-              uri: 'https://storage.googleapis.com/nexzinc/2019/06/Facebook-Icon-3-600x600.png',
-            }}
-          />
-          <Image
-            alt="1"
-            style={{ width: 40, height: 40 }}
-            source={{
-              uri: 'https://imagepng.org/wp-content/uploads/2019/08/google-icon.png',
-            }}
-          />
-        </View>
-        <View style={{ flexDirection: 'row', marginTop: 5 }}>
-          <Text style={{ fontSize: 14 }}>Don’t you have an account?</Text>
-          <Text style={{ fontSize: 14, color: 'red' }}>Register</Text>
-        </View>
+        <Button my={2} onPress={onSubmit}>
+          Login
+        </Button>
+        <Button my={2} onPress={logout}>
+          Logout
+        </Button>
+        <Button my={2} onPress={fetchData}>
+          Me
+        </Button>
       </VStack>
     </Center>
   );
