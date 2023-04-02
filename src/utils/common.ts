26a8112904a18757253a78a197d1cb6f24193166
levanchung.it <levanchung.it@gmail.com>
2023-04-02 23:06:16 +0700

    add formatDate

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,13 @@
+import moment from 'moment';
+
 //create function format number currency viet nam
 export function formatNumberCurrencyVN(number: number) {
   return number.toLocaleString('vi-VN', {
     style: 'currency',
     currency: 'VND',
   });
+}
+
+export function formatDate(date: string) {
+  return moment(date).format('DD/MM/YYYY');
 }
