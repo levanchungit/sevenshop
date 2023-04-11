3701a6c44602fccbec00ed640aca71ef06a97aef
Khoa <101702506+Khoattdps19321@users.noreply.github.com>
2023-04-11 10:25:14 +0700

    delete text in login

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -23,8 +23,8 @@ const LoginScreen = React.memo(() => {
   const { t } = useTranslation();
   const navigation = useNavigation<AppNavigationProp>();
 
-  const [email, setEmail] = useState('khoattdps19321@fpt.edu.vn');
-  const [password, setPassword] = useState('321');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = useCallback(async () => {
