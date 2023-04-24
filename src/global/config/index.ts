eba97f5d6fa1226847f885e2f44cbdef6507be89
levanchung.it <86552382+levanchung0808@users.noreply.github.com>
2023-04-25 00:10:27 +0700

    change URL HOST

--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -1,6 +1,6 @@
 //HOST BUILD || TEST LOCAL
-// export const API_URL = 'https://sevenshop.herokuapp.com';
+export const API_URL = 'https://sevenshop.herokuapp.com';
 
-export const API_URL = 'http://192.168.1.7:3000';
+// export const API_URL = 'http://192.168.1.7:3000';
 
 console.info('API_URL => ', API_URL);
