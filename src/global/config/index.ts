aa63456dfc69c1f97fa35ba869196a332a7bc93b
levanchung.it <86552382+levanchung0808@users.noreply.github.com>
2023-04-30 22:36:39 +0700

    fix API_URL

--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -1,6 +1,3 @@
-//HOST BUILD || TEST LOCAL
-export const API_URL = 'https://sevenshop.herokuapp.com';
-
-// export const API_URL = 'http://192.168.1.7:3000';
+export const API_URL = 'http://192.168.1.220:3000';
 
 console.info('API_URL => ', API_URL);
